5fa31109834cf6021fc2f3531773fab4922f5c41
Chris Cornutt <ccornutt@duosecurity.com>
2020-12-02 09:52:15 -0700

    updating day 1 with the solution for part 2

--- day1.php
+++ day1.php
@@ -2,6 +2,8 @@
 
 $lines = file(__DIR__.'/day1-input.txt');
 
+/* Finds the solution for Part 1 */
+echo "###### Part 1 ##########\n";
 foreach ($lines as $line1) {
     // Take this one and add each other line
     foreach ($lines as $line2) {
@@ -10,5 +12,18 @@ foreach ($lines as $line1) {
             echo trim($line1).' * '.trim($line2)." => ".((int)$line1*(int)$line2)."\n";
         }
 
+    }
+}
+echo "\n";
+
+/* Finds the solution for Part 2 */
+echo "###### Part 2 ##########\n";
+foreach ($lines as $line1) {
+    foreach ($lines as $line2) {
+        foreach ($lines as $line3) {
+            if ((int)$line1 + (int)$line2 + (int)$line3 == 2020) {
+                echo trim($line1).' * '.trim($line2)." * ".trim($line3)." => ".((int)$line1*(int)$line2*(int)$line3)."\n";
+            }    
+        }
     }
 }
